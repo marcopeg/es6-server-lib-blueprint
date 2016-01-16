c32a8aff508c733f95ece8b06bfbf69d061cd486
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-01-16 11:36:49 +0100

    transpire into /lib

---
+++ src/classes.js
@@ -0,0 +1,17 @@
+
+class Foo {
+
+    static prop1 = 'aaa';
+
+    state = {
+        foo: 1
+    };
+
+    constructor() {
+        console.log('construct foo');
+    }
+
+    doSomething() {
+        console.log('haha');
+    }
+}
